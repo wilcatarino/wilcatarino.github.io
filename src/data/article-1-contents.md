f254229eeda8e9d9090c3ee0431d84d7320edd11
Wilson Tavares <wilson@estoca.com.br>
2022-05-06 11:23:33 -0300

    [feature] update profile

---
+++ src/data/article-1-contents.md
@@ -0,0 +1,8 @@
+<h2>Título do Artigo</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Donec a diam lectus. Set sit amet ipsum mauris</p>
+<h3>Subtítulo 1</h3>
+<p>Donec ut librero sed accu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+<p>Pelientesque auctor nisi id magna consequat sagittis.</p>
+<h3>Subtítulo 2</h3>
+<p>Donec viverra mi quis quam pulvinar at malesuada arcu rhoncus.</p>
+<p>Vivamus fermentum semper porta. Nunc diam velit, adipscing ut tristique vitae sagittis vel odio. Maecenas convallis ullamcorper ultricied.</p>
---
+++ src/js/index.js
@@ -0,0 +1,53 @@
+const processArticlesEnabled = false;
+const articlesSource = "./data/articles.json";
+const articleTagFormat = "<span class='fa-solid fa-tag'></span><a href='#{{articleTagHref}}'>#{{articleTagName}}</a>";
+const fetchJsonOptions = {
+    method: "GET",
+    headers: {
+        "Content-Type": "application/json"
+    }
+};
+const fetchMarkdownOptions = {
+    method: "GET",
+    headers: {
+        "Content-Type": "text/markdown"
+    }
+};
+
+if (processArticlesEnabled) {
+    fetch(articlesSource, fetchJsonOptions)
+        .then(response => { return response.json(); })
+        .then(json => proccessArticles(json));
+}
+
+function enableAsideContent() {
+    let asideContent = document.getElementById("aside-content");
+    asideContent.removeAttribute("style");
+};
+
+function proccessArticles(articles) {
+    articles.forEach(article => {
+        fetch(article.content_source, fetchMarkdownOptions)
+            .then(response => { return response.text(); })
+            .then(markdown => processArticleContent(markdown));
+
+        fetch(article.tags_source, fetchJsonOptions)
+            .then(response => { return response.json(); })
+            .then(json => processArticleTags(json));
+    });
+};
+
+function processArticleContent(content) {
+    let articleContent = document.getElementById("article-content");
+    articleContent.innerHTML = marked.parse(content);
+};
+
+function processArticleTags(tags) {
+    enableAsideContent();
+    let asideTags = document.getElementById("aside-tags");
+    tags.forEach(tag => {
+        let newTag = document.createElement("li");;
+        newTag.innerHTML = articleTagFormat.replace("{{articleTagHref}}", tag).replace("{{articleTagName}}", tag);
+        asideTags.appendChild(newTag);
+    });
+};
